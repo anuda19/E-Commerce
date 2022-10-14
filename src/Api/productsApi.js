7207ea5fc5623641a44bbe688105a0e95921106b
Anurag <anuragsinha120@gmail.com>
2022-10-14 23:29:32 +0530

    added

--- src/Api/productsApi.js
+++ src/Api/productsApi.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const BASE_URL = process.env.REACT_APP_BASE_URL2
+// const BASE_URL = process.env.REACT_APP_BASE_URL2
 // const BASE_URL2 = process.env.REACT_APP_BASE_URL2
 
 // export const getProducts = async()=>{
@@ -8,22 +8,22 @@ const BASE_URL = process.env.REACT_APP_BASE_URL2
 // }
 
 export const getAmazonProducts = async()=>{
-    const getUrl = `${BASE_URL}/products`
+    const getUrl = `https://instacart-in.herokuapp.com/products`
     return await axios.get(getUrl)
 }
 
 export const addAmazonProducts = async(data)=>{
-    const getUrl = `${BASE_URL}/products/create-products`
+    const getUrl = `https://instacart-in.herokuapp.com/products/create-products`
     return await axios.post(getUrl, data)
 }
 
 
 export const delAmazonProducts = async(id)=>{
-    const delUrl = `${BASE_URL}/products/${id}`
+    const delUrl = `https://instacart-in.herokuapp.com/products/${id}`
     return await axios.delete(delUrl, id)
 }
 
 export const editAmazonProducts = async(id, data)=>{
-    const putUrl = `${BASE_URL}/products/${id}`
+    const putUrl = `https://instacart-in.herokuapp.com/products/${id}`
     return await axios.put(putUrl, id, data)
 }
